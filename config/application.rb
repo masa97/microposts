9c948b21d4e96e5670039a1feb38d69451a66476
masato <akiba.johnny97@gmail.com>
2017-01-14 12:21:14 +0000

    add top page

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Microposts
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    #追記したところ
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
